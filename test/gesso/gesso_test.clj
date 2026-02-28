0e6afa86c4e17730859ce6b4347cfe330a7bc789
Michael Rose <michael@rosenetwork.net>
2026-02-27 19:24:05 -0800

    initial commit

---
+++ test/gesso/gesso_test.clj
@@ -0,0 +1,7 @@
+(ns gesso.gesso-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [gesso.gesso :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
