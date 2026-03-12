8b7f53f91c7ebf4717a073a7963d9c84ef63e466
Michael Rose <michael@rosenetwork.net>
2026-03-11 17:53:07 -0700

    type and removed not needed includes

--- src/gesso/core.clj
+++ src/gesso/core.clj
@@ -1,12 +1,11 @@
 (ns gesso.core
-  (:require [clojure.string :as str]
-            [gesso.util :refer :all]
-            [gesso.components.accordion :as accordion]
-            [gesso.components.button :as button]
-            [gesso.components.card :as card]
-            ))
+  (:require
+   [gesso.components.accordion :as accordion]
+   [gesso.components.button :as button]
+   [gesso.components.card :as card]))
 
 ;; public api
+
 (def accordion accordion/accordion)
 (def accordion-item accordion/accordion-item)
 (def accordion-trigger accordion/accordion-trigger)
@@ -15,9 +14,8 @@
 (def button button/button)
 
 (def card card/card)
-(def card-titlie card/card-title)
+(def card-title card/card-title)
 (def card-description card/card-description)
 (def card-header card/card-header)
 (def card-content card/card-content)
-(def card-footer card/card-footer)
 (def card-footer card/card-footer)
