8be5a53e94f0ed4de81ba0c9bed8bb2b2ce82327
Michael Rose <michael@rosenetwork.net>
2026-03-24 02:53:47 -0700

    moved themes and redid a more general themeing

--- src/gesso/build/themes.clj
+++ src/gesso/build/themes.clj
@@ -3,69 +3,122 @@
    [babashka.fs :as fs]
    [clojure.string :as str]))
 
-(defn- theme-name-from-path
+(def ^:private axis-specs
+  [{:axis :color-theme
+    :dir "color-theme"
+    :attr "data-color-theme"
+    :requires-dark? true}
+
+   {:axis :density
+    :dir "density"
+    :attr "data-density"
+    :requires-dark? false}
+
+   {:axis :typography
+    :dir "typography"
+    :attr "data-typography"
+    :requires-dark? false}
+
+   {:axis :shape
+    :dir "shape"
+    :attr "data-shape"
+    :requires-dark? false}])
+
+(defn- preset-name-from-path
   [path]
   (-> path fs/file-name str (str/replace #"\.css$" "")))
 
+(defn- find-matching-brace
+  [s open-idx]
+  (loop [i (inc open-idx)
+         depth 1]
+    (when (< i (count s))
+      (case (.charAt ^String s i)
+        \{ (recur (inc i) (inc depth))
+        \} (if (= depth 1)
+             i
+             (recur (inc i) (dec depth)))
+        (recur (inc i) depth)))))
+
 (defn- extract-block
   [css selector]
-  (let [idx (str/index-of css selector)]
-    (when (some? idx)
-      (let [open-idx (str/index-of css "{" idx)]
-        (when (some? open-idx)
-          (loop [i (inc open-idx)
-                 depth 1]
-            (when (< i (count css))
-              (let [ch (.charAt ^String css i)]
-                (cond
-                  (= ch \{)
-                  (recur (inc i) (inc depth))
+  (when-let [idx (str/index-of css selector)]
+    (when-let [open-idx (str/index-of css "{" idx)]
+      (when-let [close-idx (find-matching-brace css open-idx)]
+        (subs css idx (inc close-idx))))))
 
-                  (= ch \})
-                  (if (= depth 1)
-                    (subs css idx (inc i))
-                    (recur (inc i) (dec depth)))
-
-                  :else
-                  (recur (inc i) depth))))))))))
-
-(defn- strip-theme-inline-block
+(defn- strip-theme-inline-blocks
   [css]
-  (if-let [block (extract-block css "@theme inline")]
-    (str/replace css block "")
-    css))
+  (loop [css css]
+    (if-let [block (extract-block css "@theme inline")]
+      (recur (str/replace css block ""))
+      css)))
 
 (defn- rewrite-root-block
-  [theme-name block]
+  [attr preset-name block]
   (str/replace block
                #":root\s*\{"
-               (str "html[data-theme=\"" theme-name "\"] {")))
+               (str "html[" attr "~=\"" preset-name "\"] {")))
 
 (defn- rewrite-dark-block
-  [theme-name block]
+  [attr preset-name block]
   (str/replace block
                #"\.dark\s*\{"
-               (str "html.dark[data-theme=\"" theme-name "\"] {")))
+               (str "html.dark[" attr "~=\"" preset-name "\"] {")))
 
-(defn- theme-css
-  [path]
-  (let [theme-name (theme-name-from-path path)
-        css        (-> (slurp (str path))
-                       strip-theme-inline-block)
-        root-block (extract-block css ":root")
-        dark-block (extract-block css ".dark")]
+(defn- preset-css
+  [{:keys [axis attr requires-dark?]} path]
+  (let [preset-name (preset-name-from-path path)
+        css         (-> (slurp (str path))
+                        strip-theme-inline-blocks)
+        root-block  (extract-block css ":root")
+        dark-block  (extract-block css ".dark")]
     (when-not root-block
-      (throw (ex-info "Missing :root block" {:file (str path)})))
-    (when-not dark-block
-      (throw (ex-info "Missing .dark block" {:file (str path)})))
-    (str "/* " theme-name " */\n"
-         (rewrite-root-block theme-name root-block)
-         "\n\n"
-         (rewrite-dark-block theme-name dark-block)
+      (throw (ex-info "Missing :root block"
+                      {:axis axis
+                       :file (str path)})))
+    (when (and requires-dark? (nil? dark-block))
+      (throw (ex-info "Missing .dark block"
+                      {:axis axis
+                       :file (str path)})))
+    (str "/* " (name axis) ": " preset-name " */\n"
+         (rewrite-root-block attr preset-name root-block)
+         (when dark-block
+           (str "\n\n"
+                (rewrite-dark-block attr preset-name dark-block)))
          "\n")))
 
+(defn- css-files-in-dir
+  [dir]
+  (if (fs/exists? dir)
+    (->> (fs/list-dir dir)
+         (filter #(and (fs/regular-file? %)
+                       (str/ends-with? (str %) ".css")))
+         (sort-by str))
+    []))
+
+(defn- discover-presets
+  [input-dir]
+  (mapcat
+   (fn [{:keys [dir] :as spec}]
+     (let [preset-dir (fs/path input-dir dir)]
+       (map (fn [path] {:spec spec :path path})
+            (css-files-in-dir preset-dir))))
+   axis-specs))
+
 (defn build!
-  "Reads raw theme files from :input-dir and writes a generated CSS bundle to :output-file.
+  "Reads raw preset CSS files from :input-dir and writes a generated CSS bundle
+  to :output-file.
+
+  Supported preset axes are discovered by directory:
+
+    resources/themes/color-theme/*.css
+    resources/themes/density/*.css
+    resources/themes/typography/*.css
+    resources/themes/shape/*.css
+
+  Color-theme files require both :root and .dark blocks.
+  Other preset files require :root and may optionally include .dark.
 
   Defaults are intended for the gesso repo itself:
     input-dir   => resources/themes
@@ -77,24 +130,24 @@
   ([{:keys [input-dir output-file]
      :or   {input-dir "resources/themes"
             output-file "resources/public/gesso/themes.css"}}]
-   (let [theme-dir (fs/path input-dir)]
-     (when-not (fs/exists? theme-dir)
+   (let [input-dir (fs/path input-dir)]
+     (when-not (fs/exists? input-dir)
        (throw (ex-info "Theme input directory does not exist"
-                       {:input-dir input-dir})))
-     (let [files (->> (fs/list-dir theme-dir)
-                      (filter #(and (fs/regular-file? %)
-                                    (str/ends-with? (str %) ".css")))
-                      (sort-by str))]
-       (when (empty? files)
-         (throw (ex-info "No theme files found"
-                         {:input-dir input-dir})))
+                       {:input-dir (str input-dir)})))
+     (let [presets (discover-presets input-dir)]
+       (when (empty? presets)
+         (throw (ex-info "No preset CSS files found"
+                         {:input-dir (str input-dir)})))
        (fs/create-dirs (fs/parent output-file))
        (spit output-file
-             (str (str/join "\n\n" (map theme-css files))
+             (str (str/join "\n\n"
+                            (map (fn [{:keys [spec path]}]
+                                   (preset-css spec path))
+                                 presets))
                   "\n"))
        (println "Wrote" output-file)
-       (doseq [f files]
-         (println " -" (theme-name-from-path f)))))))
+       (doseq [{:keys [spec path]} presets]
+         (println " -" (name (:axis spec)) (preset-name-from-path path)))))))
 
 (defn -main
   [& _]
--- src/gesso/theme.clj
+++ src/gesso/theme.clj
@@ -1,70 +1,143 @@
 (ns gesso.theme
   (:require [clojure.string :as str]))
 
-(defn- normalize-theme [theme]
-  (cond
-    (nil? theme) nil
-    (keyword? theme) (name theme)
-    :else (str theme)))
+(def ^:private axis-specs
+  [{:axis :color-theme
+    :attr :data-color-theme
+    :meta-name "gesso-color-theme-default"}
 
-(defn- normalize-mode [mode]
+   {:axis :density
+    :attr :data-density
+    :meta-name "gesso-density-default"}
+
+   {:axis :typography
+    :attr :data-typography
+    :meta-name "gesso-typography-default"}
+
+   {:axis :shape
+    :attr :data-shape
+    :meta-name "gesso-shape-default"}])
+
+(defn- normalize-token
+  [x]
+  (cond
+    (nil? x) nil
+    (keyword? x) (name x)
+    :else (str x)))
+
+(defn- normalize-axis-value
+  [v]
+  (cond
+    (nil? v) nil
+    (sequential? v) (->> v
+                         (map normalize-token)
+                         (remove str/blank?)
+                         (str/join " "))
+    :else (normalize-token v)))
+
+(defn- normalize-mode
+  [mode]
   (cond
     (nil? mode) nil
     (keyword? mode) (name mode)
     :else (-> mode str str/lower-case)))
 
+(defn- normalize-theme-config
+  [theme-or-config]
+  (let [config (cond
+                 (nil? theme-or-config) {}
+                 (map? theme-or-config) theme-or-config
+                 :else {:color-theme theme-or-config})]
+    (reduce
+     (fn [m {:keys [axis]}]
+       (if-let [v (normalize-axis-value (get config axis))]
+         (assoc m axis v)
+         m))
+     {}
+     axis-specs)))
+
 (defn html-theme-attrs
   "Return attrs intended for the html/root theme state.
 
-  Examples:
-    (html-theme-attrs \"vercel\" :system)
-    ;; => {:data-theme \"vercel\" :data-theme-mode \"system\"}
+  Supported forms:
 
-    (html-theme-attrs \"vercel\" :dark)
-    ;; => {:data-theme \"vercel\" :data-theme-mode \"dark\" :class \"dark\"}"
-  ([theme]
-   (html-theme-attrs theme nil))
-  ([theme mode]
-   (let [theme (normalize-theme theme)
-         mode  (normalize-mode mode)]
+    (html-theme-attrs \"vercel\" :system)
+    ;; => {:data-color-theme \"vercel\" :data-theme-mode \"system\"}
+
+    (html-theme-attrs
+      {:color-theme \"vercel\"
+       :density :comfortable
+       :typography :ui}
+      :dark)
+    ;; => {:data-color-theme \"vercel\"
+    ;;     :data-density \"comfortable\"
+    ;;     :data-typography \"ui\"
+    ;;     :data-theme-mode \"dark\"
+    ;;     :class \"dark\"}
+
+  Axis values may be a single keyword/string or a vector/seq. Sequential values
+  are joined with spaces, which pairs with CSS selectors that use ~= matching.
+
+    {:density [:compact :compactish]}
+    ;; => {:data-density \"compact compactish\"}"
+  ([theme-or-config]
+   (html-theme-attrs theme-or-config nil))
+  ([theme-or-config mode]
+   (let [config (normalize-theme-config theme-or-config)
+         mode   (normalize-mode mode)]
      (cond-> {}
-       theme (assoc :data-theme theme)
+       (:color-theme config) (assoc :data-color-theme (:color-theme config))
+       (:density config) (assoc :data-density (:density config))
+       (:typography config) (assoc :data-typography (:typography config))
+       (:shape config) (assoc :data-shape (:shape config))
        mode (assoc :data-theme-mode mode)
        (= mode "dark") (assoc :class "dark")))))
 
-#_(defn theme
-  "Return a base-html-friendly map for fixed defaults.
-
-  Example:
-    (merge (theme \"vercel\" :system) ...)"
-  ([theme]
-   {:base/html-attrs (html-theme-attrs theme nil)})
-  ([theme mode]
-   {:base/html-attrs (html-theme-attrs theme mode)}))
-
 (defn theme-head
-  "Return head elements declaring default theme/mode for gesso-theme.js.
+  "Return head elements declaring default theme axes/mode for gesso-theme.js.
 
-  Example:
-    (theme-head \"vercel\" :system)"
-  ([theme]
-   (theme-head theme nil))
-  ([theme mode]
-   (let [theme (normalize-theme theme)
-         mode  (normalize-mode mode)]
-     (remove nil?
-             [[:meta {:name "gesso-theme-default"
-                      :content theme}]
-              (when mode
-                [:meta {:name "gesso-theme-mode-default"
-                        :content mode}])]))))
+  Examples:
+    (theme-head \"vercel\" :system)
 
+    (theme-head {:color-theme \"vercel\"
+                 :density :comfortable}
+                :dark)"
+  ([theme-or-config]
+   (theme-head theme-or-config nil))
+  ([theme-or-config mode]
+   (let [config (normalize-theme-config theme-or-config)
+         mode   (normalize-mode mode)]
+     (remove
+      nil?
+      (concat
+       (for [{:keys [axis meta-name]} axis-specs
+             :let [value (get config axis)]
+             :when value]
+         [:meta {:name meta-name
+                 :content value}])
+       [(when mode
+          [:meta {:name "gesso-theme-mode-default"
+                  :content mode}])])))))
 
 (defn theme
-  "Returns a map compatible with Biff's base-html that sets
-   both the initial HTML attributes AND the JS configuration tags."
-  ([theme-name]
-   (theme theme-name :system))
-  ([theme-name mode]
-   {:base/html-attrs (html-theme-attrs theme-name mode)
-    :base/head (theme-head theme-name mode)}))
+  "Returns a map compatible with Biff's base-html that sets both the initial
+  HTML attributes AND the JS configuration tags.
+
+  Convenience color-theme usage:
+    (theme \"vercel\")
+    (theme \"vercel\" :system)
+
+  Multi-axis usage:
+    (theme {:color-theme \"vercel\"
+            :density :comfortable
+            :typography :ui
+            :shape :default})
+
+    (theme {:color-theme \"vercel\"
+            :density [:compact :compactish]}
+           :dark)"
+  ([theme-or-config]
+   (theme theme-or-config :system))
+  ([theme-or-config mode]
+   {:base/html-attrs (html-theme-attrs theme-or-config mode)
+    :base/head (theme-head theme-or-config mode)}))
