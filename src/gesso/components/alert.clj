e4e5277abc37638a24e0b06937e3a110d74e052e
Michael Rose <michael@rosenetwork.net>
2026-03-26 19:38:03 -0700

    revised themes and alert

--- src/gesso/components/alert.clj
+++ src/gesso/components/alert.clj
@@ -1,34 +1,57 @@
 (ns gesso.components.alert
-  (:require [gesso.util :refer :all]))
+  (:require
+   [gesso.util :refer :all]
+   [gesso.components.text :as text]))
 
 (def ^:private alert-classes
   {:default "alert"
    :destructive "alert-destructive"})
 
 (defn alert-title
-  "Alert title subcomponent."
+  "Alert title subcomponent.
+
+   Uses the shared text system for typography without introducing additional
+   component-specific theme classes."
   [& args]
   (if (only-map-arg? args)
     (let [{:keys [props class attrs]} (split-opts (first args))]
-      (el :strong
-          {:class (class-names "alert-title" class)
-           :data-title true}
-          attrs
-          (nodes (:text props))))
+      (text/text
+       {:variant :label
+        :as :strong
+        :class class
+        :attrs (merge-attrs attrs {:data-title true})
+        :text (:text props)}))
     (let [[opts children] (normalize-component-args args)
           {:keys [class attrs]} (split-opts opts)]
-      (el :strong
-          {:class (class-names "alert-title" class)
-           :data-title true}
-          attrs
-          children))))
+      (apply text/text
+             {:variant :label
+              :as :strong
+              :class class
+              :attrs (merge-attrs attrs {:data-title true})}
+             children))))
 
 (defn alert-content
-  "Alert content subcomponent."
+  "Alert content subcomponent.
+
+   Uses the shared body text system without introducing additional
+   component-specific theme classes."
   [& args]
-  (let [[opts children] (normalize-component-args args)
-        {:keys [class attrs]} (split-opts opts)]
-    (el :section {:class (class-names "alert-content" class)} attrs children)))
+  (if (only-map-arg? args)
+    (let [{:keys [props class attrs]} (split-opts (first args))]
+      (text/text
+       {:variant :body
+        :as :section
+        :class class
+        :attrs attrs
+        :text (:text props)}))
+    (let [[opts children] (normalize-component-args args)
+          {:keys [class attrs]} (split-opts opts)]
+      (apply text/text
+             {:variant :body
+              :as :section
+              :class class
+              :attrs attrs}
+             children))))
 
 (defn alert
   "Alert component.
@@ -52,11 +75,12 @@
           {:class (class-names cls class)
            :role "alert"}
           attrs
-          [(when title (alert-title {:text title}))
+          [(when title
+             (alert-title {:text title}))
            (when content
              (if (and (sequential? content) (not (hiccup-element? content)))
                (apply alert-content {} content)
-               (alert-content {} content)))]))
+               (alert-content {:text content}))) ]))
     (let [[opts children] (normalize-component-args args)
           {:keys [props class attrs]} (split-opts opts)
           variant (or (:variant props) :default)
