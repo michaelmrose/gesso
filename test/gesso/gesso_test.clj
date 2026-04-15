a93888af2795a0d69d4160f9d05482a80730a7f2
Michael Rose <michael@rosenetwork.net>
2026-04-15 02:16:41 -0700

    redundant

--- test/gesso/gesso_test.clj
+++
@@ -1,7 +0,0 @@
-(ns gesso.gesso-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [gesso.gesso :as sut])) ; system under test
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
